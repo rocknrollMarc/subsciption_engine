c6101b63628915dea11ad0af706652b5f2fb51a3
Marc <marc.bluemner@gmail.com>
2014-10-27 13:02:09 +0100

    add Warden middleware

--- lib/subscriber/engine.rb
+++ lib/subscriber/engine.rb
@@ -1,6 +1,19 @@
 module Subscriber
+  require 'warden'
   class Engine < ::Rails::Engine
     isolate_namespace Subscriber
+
+    initializer "subscriber.middleware.warden" do
+      Rails.application.config.middleware.use Warden::Manager do |manager|
+        manager.serialize_into_session do |user|
+          user.id
+        end
+
+        manager.serialize_from_session do |id|
+          Subscriber::User.find(id)
+        end
+      end
+    end
 
     config.generators do |g|
       g.test_framework :rspec, :view_spec => false
