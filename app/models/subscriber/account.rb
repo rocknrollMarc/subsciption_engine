645f6bb3e2c4df88ca1fc028ba0902bd31c9d98b
Marc <marc.bluemner@gmail.com>
2014-10-27 15:13:09 +0100

    subdomain exclusion

--- app/models/subscriber/account.rb
+++ app/models/subscriber/account.rb
@@ -4,5 +4,7 @@ module Subscriber
     accepts_nested_attributes_for :owner
 
     validates :subdomain, :presence => true, :uniqueness => true
+    validates_exclution_of :subdomain, :in => EXCLUDED_SUBDOMAINS,
+      :message => "is not allowed. Please choose another subdomain."
   end
 end
