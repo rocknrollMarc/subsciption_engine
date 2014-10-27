4948439484a03fef2af2c5d04623f96bb8e2ac4e
Marc <marc.bluemner@gmail.com>
2014-10-27 14:46:09 +0100

    validates subdomain uniqueness and rpesence  => true

--- app/models/subscriber/account.rb
+++ app/models/subscriber/account.rb
@@ -2,5 +2,7 @@ module Subscriber
   class Account < ActiveRecord::Base
     belongs_to :owner, :class_name => "Subscriber::User"
     accepts_nested_attributes_for :owner
+
+    validates :subdomain, :presence => true, :uniqueness => true
   end
 end
