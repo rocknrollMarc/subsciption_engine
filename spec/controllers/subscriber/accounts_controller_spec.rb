bf38e3bea2128f51201fef18da8157ef77ac4c80
Marc <marc.bluemner@gmail.com>
2014-10-27 13:44:51 +0100

    added account

--- spec/controllers/subscriber/accounts_controller_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-module Subscriber
-  RSpec.describe AccountsController, :type => :controller do
-
-  end
-end
--- spec/controllers/subscriber/dashboard_controller_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-module Subscriber
-  RSpec.describe DashboardController, :type => :controller do
-
-  end
-end
