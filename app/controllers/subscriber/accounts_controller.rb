2bd6cef97d394ccd8d2e3b8d9dde02084f89ecf8
Marc <marc.bluemner@gmail.com>
2014-10-27 14:30:50 +0100

    root_url(:subdomain => account.subdomain)

--- app/controllers/subscriber/accounts_controller.rb
+++ app/controllers/subscriber/accounts_controller.rb
@@ -15,13 +15,15 @@ module Subscriber
       p account.owner
       env["warden"].set_user(account, :scope => :account)
       flash[:success] = "Your account has been successfully created."
-      redirect_to subscriber.root_url
+      redirect_to subscriber.root_url(:subdomain => account.subomain)
     end
 
   private
 
       def account_params
-        params.require(:account).permit(:name, :subdomain, { :owner_attributes => [ :email, :password, :password_confirmation ]})
+        params.require(:account).permit(:name, :subdomain, 
+                                        { :owner_attributes => 
+                                          [ :email, :password, :password_confirmation ]})
       end
 
     end
