7ba0ed6a15cd2ab20970fe6f56ce70d996a74a21
Marc <marc.bluemner@gmail.com>
2014-10-27 15:11:25 +0100

    isubdomain with restricted name

--- spec/features/accounts/sign_up_spec.rb
+++ spec/features/accounts/sign_up_spec.rb
@@ -37,4 +37,20 @@ feature "Accounts" do
     expect(page).to have_content("Sorry, your account could not be created.")
     expect(page).to have_content("Subdomain has already been taken")
   end
+
+  scenario "Subdomain with restricted name" do
+    visit subscriber.root_path
+    click_link "Account Sign Up"
+
+    fill_in "Name", :with => "Test"
+    fill_in "Subdomain", :with => "test"
+    fill_in "Email", :with => "subscriber@example.com"
+    fill_in "Password", :with => "password"
+    fill_in "Password confirmation", :with => "password"
+    click_button "Create Account"
+
+    expect(page.current_url).to eq("http://www.example.com/subscriber/accounts")
+    expect(page).to have_content("Sorry, your account could not be created.")
+    expect(page).to have_content("Subdomain has already been taken")
+  end
 end
