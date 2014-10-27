d9a805f4645d3d0c88c7187e0bc908c2b2510ed7
Marc <marc.bluemner@gmail.com>
2014-10-27 14:41:11 +0100

    added subdomain uniqueness spec

--- spec/features/accounts/sign_up_spec.rb
+++ spec/features/accounts/sign_up_spec.rb
@@ -19,4 +19,22 @@ feature "Accounts" do
 
     expect(page).to have_content(success_message)
   end
+
+  scenario "Ensure subdomain uniquieness" do
+    Subscriber::Account.create!(:subdomain => "test", :name => "Test")
+    visit subscriber.root_path
+    click_link "Account Sign Up"
+
+    fill_in "Name", :with => "Test"
+    fill_in "Subdomain", :with => "test"
+    fill_in "Email", :with => "subscriber@example.com"
+    fill_in "Password", :with => "password", :exact => true
+    fill_in "Password confirmation", :with => "password"
+
+    click_button "Create Account"
+
+    expect(page.current_url).to eq("http://www.example.com/subscriber/accounts")
+    expect(page).to have_content("Sorry, your account could not be created.")
+    expect(page).to have_content("Subdomain has already been taken")
+  end
 end
