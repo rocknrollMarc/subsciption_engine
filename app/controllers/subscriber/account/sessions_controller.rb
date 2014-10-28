6a1e8aa8751e2d37ce8241a0d4f7d98d6ed5ce3f
Marc <marc.bluemner@gmail.com>
2014-10-28 12:28:21 +0100

    sessions#new

--- app/controllers/subscriber/account/sessions_controller.rb
+++ app/controllers/subscriber/account/sessions_controller.rb
@@ -2,5 +2,10 @@ require_dependency "subscriber/application_controller"
 
 module Subscriber
   class Account::SessionsController < ApplicationController
+
+    def new
+      @user = User.new
+    end
+
   end
 end
