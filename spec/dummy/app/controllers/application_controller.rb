7602388d7190fd4f5bcb688660604cd12460dc83
Marc <marc.bluemner@gmail.com>
2014-10-28 15:32:24 +0100

    force_authentication!

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def force_authentication!(account, user)
+    env["warden"].set_user(user, :scope => :user)
+    env["warden"].set_user(account, :scope => :account)
+  end
 end
