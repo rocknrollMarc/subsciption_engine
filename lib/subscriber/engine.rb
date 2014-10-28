c682f1b54480a779785416fcdbb88149631745a1
Marc <marc.bluemner@gmail.com>
2014-10-28 12:47:03 +0100

    warden default strategy :password

--- lib/subscriber/engine.rb
+++ lib/subscriber/engine.rb
@@ -6,6 +6,10 @@ module Subscriber
 
     initializer "subscriber.middleware.warden" do
       Rails.application.config.middleware.use Warden::Manager do |manager|
+
+        # use :password as default warden strategy
+        manager.default_strategies :password
+
         manager.serialize_into_session do |user|
           user.id
         end
