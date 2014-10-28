dff6fd685b8a29e070836cf819436208d71d8368
Marc <marc.bluemner@gmail.com>
2014-10-28 15:26:55 +0100

    user params and create

--- app/controllers/subscriber/account/users_controller.rb
+++ app/controllers/subscriber/account/users_controller.rb
@@ -6,5 +6,20 @@ module Subscriber
     def new
       @user = Subscriber::User.new
     end
+
+    def create
+      account = Subscriber::Account.find_by!(:subdomain => request.subdomain)
+      user = account.users.create(user_params)
+      env["warden"].set_user(user, :scope => :user)
+      env["warden"].set_user(account, :scope => :account)
+      flash[:success] = "You have signed up successfully."
+      redirect_to root_path
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
   end
 end
