1e1de1688871060e41fe5b2a94258fc0e859e640
Marc <marc.bluemner@gmail.com>
2014-10-28 12:49:04 +0100

    sessions#create action

--- app/controllers/subscriber/account/sessions_controller.rb
+++ app/controllers/subscriber/account/sessions_controller.rb
@@ -7,5 +7,12 @@ module Subscriber
       @user = User.new
     end
 
+    def create
+      if env["warden"].authenticate(:scope => :user)
+        flash[:notice] = "You are now signed in."
+        redirect_to root_path
+      end
+    end
+
   end
 end
