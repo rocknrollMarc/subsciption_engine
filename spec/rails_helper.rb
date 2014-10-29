741cb9c581cab16e49a3cf4e5af3ac9083c93858
Marc <marc.bluemner@gmail.com>
2014-10-29 11:06:16 +0100

    fixed rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -26,6 +26,11 @@ Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.include AuthenticationHelpers, :type => :feature
+  config.after :type => :feature do
+    logout
+  end
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
