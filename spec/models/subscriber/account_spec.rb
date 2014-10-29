81af2479b278d7b897d114f904884a9c5c2a61a6
Marc <marc.bluemner@gmail.com>
2014-10-29 10:00:09 +0100

    schema specs

--- spec/models/subscriber/account_spec.rb
+++ spec/models/subscriber/account_spec.rb
@@ -1,6 +1,22 @@
 require 'rails_helper'
 
 describe Subscriber::Account  do
+  def schema_exists?(account)
+    query = %Q{SELECT nspname FROM pg_namespace WHERE nspname='#{account.subdomain}'}
+    result = ActiveRecord::Base.connection.select_value(query)
+    result.present?
+  end
+
+  it 'created a schema' do
+    account = Subscriber::Account.create!({
+      :name => "First Account",
+      :subdomain => "first"
+    })
+    account.create_schema
+    failure_message = "Schema #{account.subdomain} does not exist"
+    assert schema_exists?(account), failure_message
+  end
+
   it 'can be created with an owner' do
     params = {
       :name => "Test Account",
