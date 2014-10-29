8482851fc125a07a3bd0da88b32eb20cfab55ff7
Marc <marc.bluemner@gmail.com>
2014-10-29 12:06:07 +0100

    apartment setup database.reset

--- spec/features/accounts/scoping_spec.rb
+++ spec/features/accounts/scoping_spec.rb
@@ -5,12 +5,16 @@ feature "Account scoping" do
   let!(:account_b) { FactoryGirl.create(:account_with_schema) }
 
   before do
-    Thing.create(:name => "Account A's Thing", :account => account_a)
-    Thing.create(:name => "Account B's Thing", :account => account_b)
+    Apartment::Database.switch(account_a.subdomain)
+    Thing.create(:name => "Account A's Thing")
+    Apartment::Database.switch(account_b.subdomain)
+    Thing.create(:name => "Account B's Thing")
+    Apartment::Database.reset
   end
 
   scenario "displays only account A's records" do
     sign_in_as(:user => account_a.owner, :account => account_a)
+    visit main_app.things_url(:subdomain => account_a.subdomain)
 
     visit "/things"
     expect(page).to have_content("Account A's Thing")
@@ -19,6 +23,7 @@ feature "Account scoping" do
 
   scenario "displays only account B's records" do
     sign_in_as(:user => account_b.owner, :account => account_b)
+    visit main_app.things_url(:subdomain => account_b.subdomain)
 
     visit "/things"
     expect(page).to have_content("Account B's Thing")
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,6 +31,10 @@ RSpec.configure do |config|
     logout
   end
 
+  config.after(:each) do
+    Apartment::Database.reset
+  end
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
